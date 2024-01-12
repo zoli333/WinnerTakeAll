f63a332069228b022dffdc69d8431860c0f27fda
unknown <nzg.science@gmail.com>
2024-01-12 20:16:31 +0100

    fix weightdecay

--- conv_wta/custom_optimizer.py
+++ conv_wta/custom_optimizer.py
@@ -12,9 +12,8 @@ class MomentumOptimizer(torch.optim.Optimizer):
 
     # Init Method: 
     def __init__(self, params, lr=1e-3, momentum=0.9, weight_decay=0.0):
-        super(MomentumOptimizer, self).__init__(params, defaults={'lr': lr})
+        super(MomentumOptimizer, self).__init__(params, defaults={'lr': lr, 'weight_decay': weight_decay})
         self.momentum = momentum
-        self.weight_decay = weight_decay
         self.state = dict()
         for group in self.param_groups:
             for p in group['params']:
@@ -39,6 +38,6 @@ class MomentumOptimizer(torch.optim.Optimizer):
 
                 p.add_(mom)
 
-                if self.weight_decay > 0.0:
-                    p.add_(p, alpha=(-group["lr"] * self.weight_decay))
+                if group["weight_decay"] > 0.0:
+                    p.add_(p, alpha=(-group["lr"] * group["weight_decay"]))
 
